8eb5e03dae3fe0ac17bc38429a57dc47e1d07764
mmarras <marrasmichael2499@gmail.com>
2023-08-08 09:33:55 -0700

    Script that goes through a list of names and dates in a csv file from a spreadsheet, and creates directories in their respective paths. The script also avoids making copies

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 path_2017 = 'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2017'
 path_2018 = 'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2018'
 path_2019 = 'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2019'
+counter = 0
 
 infile = open("SpreadSheet.csv", 'r')
 
@@ -11,24 +12,24 @@ for line in infile:
     last_name = split_line[0][1:]
     first_name = split_line[1][:-1]
     file_name = last_name+','+first_name
-    if os.path.exists(f'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2017\\{file_name}'):
-        print("huzzah")
-        continue
-    if os.path.exists(f'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2018\\{file_name}'):
-        print("huzzah")
-        continue
-    if os.path.exists(f'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2019\\{file_name}'):
-        print("huzzah")
-        continue
     if split_line[2].__contains__('2017'):
-        print("boo")
-        os.chdir(path_2017)
-        os.makedirs(file_name)
+        if os.path.exists(f'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2017\\{file_name}'):
+            continue
+        else:
+            print("2017")
+            os.chdir(path_2017)
+            os.makedirs(f'{file_name}')
     if split_line[2].__contains__('2018'):
-        print("apple")
-        os.chdir(path_2018)
-        os.makedirs(file_name)
+        if os.path.exists(f'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2018\\{file_name}'):
+            continue
+        else:
+            print("2018")
+            os.chdir(path_2018)
+            os.makedirs(f'{file_name}')
     if split_line[2].__contains__('2019'):
-        print("orange")
-        os.chdir(path_2019)
-        os.makedirs(file_name)
+        if os.path.exists(f'C:\\Users\\mmarras\\Desktop\\File Project\\Investigation Files 2019\\{file_name}'):
+            continue
+        else:
+            print("2019")
+            os.chdir(path_2019)
+            os.makedirs(f'{file_name}')
